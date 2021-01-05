de3a8b50b6dc983e7ee3c9372d36372722ea1cac
RodasNega <rodas.nega@ocsbstudent.ca>
2021-01-05 16:13:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+document.getElementById('response').addEventListener('click', museumEligible)
+
+document.getElementById('time').addEventListener('click', museumEligible)
+
+function museumEligible () {
+  const day = document.getElementById('time').value
+  // user inputs the day of the week
+  const age = document.getElementById('response').value
+  // user inputs their age
+  if ((day == "Tuesday" || day == "Thursday") || (age > 12 && age < 21)) {
+    alert('You are eligible for student pricing.')
+  } else {
+    alert('You must pay regular price.')
+  // conditions state that student pricing is eligible only during Tuesdays and Thursdays or between ages of 12 and 21
+  }
+}
